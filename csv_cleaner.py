1531e0725ae47446e0e0ea1d2bd846219a8e032b
Maksymilian Nowakowski <maks.nowakowski.pro@gmail.com>
2026-01-05 13:49:29 +0000

    feat: clean & convert types

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -8,3 +8,7 @@ with open("data/input.csv", newline="") as file:
     for row in reader:
         if row["name"] and row["age"] and row["score"]:
             clean_rows.append(row)
+
+    for row in clean_rows:
+        row["age"] = int(row["age"])
+        row["score"] = int(row["score"])
