4ea96b42b048eaf121f28b86b0a82dc869291463
Maksymilian Nowakowski <maks.nowakowski.pro@gmail.com>
2026-01-05 13:56:08 +0000

    feat: write clean csv

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -12,3 +12,8 @@ with open("data/input.csv", newline="") as file:
     for row in clean_rows:
         row["age"] = int(row["age"])
         row["score"] = int(row["score"])
+
+with open("output/cleaned_output.csv", "w", newline="") as file:
+    writer = csv.DictWriter(file, fieldnames=clean_rows[0].keys())
+    writer.writeheader()
+    writer.writerows(clean_rows)
