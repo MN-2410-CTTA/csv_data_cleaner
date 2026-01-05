4c07cde54d3397fe49d2d1a9daeb5598c52bb76f
Maksymilian Nowakowski <maks.nowakowski.pro@gmail.com>
2026-01-05 13:48:27 +0000

    feat: validate data

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -2,5 +2,9 @@ import csv
 
 with open("data/input.csv", newline="") as file:
     reader = csv.DictReader(file)
+
+    clean_rows = []
+
     for row in reader:
-        print(row)
+        if row["name"] and row["age"] and row["score"]:
+            clean_rows.append(row)
